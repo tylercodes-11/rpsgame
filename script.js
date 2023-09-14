928ab701f905d760a3202968c8587ed533372b29
Ty Reid <trawx11@gmail.com>
2023-09-13 20:33:55 -0400

    updated ui and increment function for app

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const selectionButtons = document.querySelectorAll('[data-selection]')
-const finalColumn = document. querySelector('[data-final-column]')
-const yourScoreSpan = document.querySelector('[data-your-score]')
-const computerScoreSpan = document.querySelector('[data-computer-score')
+const selectionButtons = document.querySelectorAll('[data-selection]');
+// const scoreBoard_div = document. querySelectorAll('.result-score');
+const yourScoreSpan = document.getElementById('userScore');
+const computerScoreSpan = document.getElementById('botScore');
+
+let userScore = 0;
+let botScore = 0;
+
 const SELECTIONS = [
     {
         name: 'rock' , //rock object
@@ -23,42 +27,69 @@ const SELECTIONS = [
 
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => { //take in an event
+        
         const selectionName = selectionButton.dataset.selection
+        console.log("this is my selection:",selectionName);
         const selection = SELECTIONS.find(selection => selection.name === selectionName) //what choice selected
         makeSelection(selection)
     })
 })
 
-function makeSelection(selection) {
-    const computerSelection = randomSelection()
-    const yourWinner = isWinner(selection, computerSelection)
-    const computerWinner = isWinner(computerSelection, selection)
-
-    addSelectionResult(computerSelection, computerWinner)
-    addSelectionResult(selection, yourWinner)
-
-    if(yourWinner) incrementScore(yourScoreSpan) //incrementing your score
-    if(computerWinner) incrementScore(computerScoreSpan) //incrementing computer score
-};
-
-
-function  incrementScore(scoreSpan) { //keeping score function
-    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1;
-}
-
-function addSelectionResult(selection, winner) {
-    const div = document.createElement('div')
-    div.innerText = selection.emoji
-    div.classList.add('result-selection')
-    if (winner) div.classList.add('winner')
-    finalColumn.after(div) //updating past input after last
-}
-
-function isWinner (selection, opponentSelection) { //winner function
-    return selection.beats === opponentSelection.name 
-}
-
 function randomSelection() { //random computer choice
     const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
     return SELECTIONS[randomIndex];
 }
+
+
+function makeSelection(selection) {
+    const computerSelection = randomSelection()
+    console.log(computerSelection);
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
+    console.log(selection)
+    console.log(isWinner) //
+
+    // addSelectionResult(computerSelection, computerWinner)
+    // addSelectionResult(selection, yourWinner)
+
+    if(yourWinner) win() //incrementing your score
+    if(computerWinner) loss()//incrementing computer score
+    // if(!yourWinner || computerWinner) draw() 
+};
+
+function isWinner (selection, opponentSelection) { //winner function
+    return (
+        selection.beats === opponentSelection.name 
+    )
+}
+// function draw (selection, opponentSelection) {
+//     return selection.name === opponentSelection.name
+// }
+
+
+
+function  incrementScore(scoreSpan) { //keeping score function
+    const currentScore = parseInt(scoreSpan.innerText);
+    scoreSpan.innerText = currentScore + 1;
+}
+function win() {
+    userScore++;
+    incrementScore(yourScoreSpan);
+console.log(userScore);
+
+}
+
+function loss() {
+    botScore++;
+    incrementScore(computerScoreSpan);
+}
+
+// console.log(win);
+// function addSelectionResult(selection, winner) {
+//     const div = document.createElement('div')
+//     div.innerText = selection.emoji
+//     div.classList.add('result-selection')
+//     if (winner) div.classList.add('winner')
+//     finalColumn.after(div) //updating past input after last
+// }
+
